5027916d9347e4d52b2b39107aa70d8cee4e36c2
Tamirat <amenguda@gmail.com>
2024-07-26 11:28:04 +0300

    Initialize Node.js project with npm and add basic setup

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+import express from "express";
+import { dbConnection } from "./database/dbConnection.js";
+import { config } from "dotenv";
+import cors from "cors";
+import { errorMiddleware } from "./middlewares/error.js";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import SongRoutes from "./routers/songs.js";
+
+const app = express();
+config({ path: "./config/config.env" });
+
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+
+app.use("/api/songs", SongRoutes);
+
+dbConnection();
+app.use(errorMiddleware);
+
+export default app;
